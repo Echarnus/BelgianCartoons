c3e2800e4b1a03d57d02e6fe29f7bf9a1a2bb348
Echarnus <kenneth.de.clercq@me.com>
2021-09-19 21:52:42 +0200

    Package updates - required for User secrets in VS 2022

--- src/BelgianCartoons/Triggers/GetCartoonistsFromTwitter.cs
+++ src/BelgianCartoons/Triggers/GetCartoonistsFromTwitter.cs
@@ -6,11 +6,13 @@ using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 
-namespace BelgianCartoons.Scraper.Functions
+namespace BelgianCartoons.Scraper.Functions.Triggers
 {
     public class GetCartoonistsFromTwitter
     {
-        private const string SCHEDULE_EXPRESSION = "* * */12 * * *";
+
+        //private const string SCHEDULE_EXPRESSION = "* * */12 * * *";
+        private const string SCHEDULE_EXPRESSION = "*/1 * * * * *";
         private readonly ILogger<GetCartoonistsFromTwitter> _logger;
         private readonly IRedditService _redditService;
         private readonly ITwitterService _twitterService;
@@ -18,160 +20,182 @@ namespace BelgianCartoons.Scraper.Functions
 
         public GetCartoonistsFromTwitter(ITwitterService twitterService, IRedditService redditService, ILogger<GetCartoonistsFromTwitter> logger)
         {
-            this._twitterService = twitterService;
-            this._redditService = redditService;
-            this._logger = logger;
+            _twitterService = twitterService;
+            _redditService = redditService;
+            _logger = logger;
         }
 
-        [FunctionName("GetSoupCatCartoons")]
-        public async Task RunGeSoupCatCartoons([TimerTrigger(SCHEDULE_EXPRESSION)] TimerInfo myTimer)
+        //[FunctionName("GetSoupCatCartoons")]
+        //public async Task RunGeSoupCatCartoons([TimerTrigger(SCHEDULE_EXPRESSION)] TimerInfo myTimer)
+        //{
+        //    var lastUpdated = myTimer?.ScheduleStatus?.Last ?? MINIMUM_DATE;
+        //    _logger.LogInformation("Last SyncTime was {SyncTime}", lastUpdated);
+
+        //    var tweets = await _twitterService.GetTweetsFromUserAsync("25047751", lastUpdated).ConfigureAwait(false);
+        //    var cartoonTweets = tweets.Where(tweet => tweet.Entities?.Hashtags?.Any(t => string.Equals(t?.Tag, "comic", StringComparison.OrdinalIgnoreCase) || string.Equals(t?.Tag, "comics", StringComparison.OrdinalIgnoreCase)) == true);
+        //    _logger.LogInformation("Found {TotalTweets} Tweets for cartoonist {Cartoonist}", cartoonTweets.Count(), "Soupcat");
+        //    foreach (var cartoon in cartoonTweets)
+        //    {
+        //        try
+        //        {
+        //            await _redditService.CreateLinkPostAsync("BelgianCartoons", $"{cartoon.Text.Split("\n")[0]}", $"https://twitter.com/soupcats/status/{cartoon.Id}?", "Soupcat");
+        //        }
+        //        catch (Exception ex)
+        //        {
+        //            _logger.LogError(ex, "Problem with processing Tweet {Tweet} for cartoonist {Cartoonist}", cartoon.Id, "Soupcat");
+        //        }
+        //    }
+        //}
+
+        //[FunctionName("GetArnulfBe")]
+        //public async Task RunGetArnulfBe([TimerTrigger(SCHEDULE_EXPRESSION)] TimerInfo myTimer)
+        //{
+        //    var lastUpdated = myTimer?.ScheduleStatus?.Last ?? MINIMUM_DATE;
+        //    _logger.LogInformation("Last SyncTime was {SyncTime}", lastUpdated);
+
+        //    var tweets = await _twitterService.GetTweetsFromUserAsync("249821479", lastUpdated).ConfigureAwait(false);
+        //    var cartoonTweets = tweets.Where(tweet => tweet.Entities?.Hashtags?.Any(t => string.Equals(t?.Tag, "cartoon", StringComparison.OrdinalIgnoreCase)) == true);
+        //    _logger.LogInformation("Found {TotalTweets} Tweets for cartoonist {Cartoonist}", cartoonTweets.Count(), "ArnulfBE");
+        //    foreach (var cartoon in cartoonTweets)
+        //    {
+        //        try
+        //        {
+        //            await _redditService.CreateLinkPostAsync("BelgianCartoons", $"{cartoon.Text.Split("\n")[0]}", $"https://twitter.com/arnulfcartoon/status/{cartoon.Id}?", "Arnulf");
+        //        }
+        //        catch (Exception ex)
+        //        {
+        //            _logger.LogError(ex, "Problem with processing Tweet {Tweet} for cartoonist {Cartoonist}", cartoon.Id, "Arnulf");
+        //        }
+        //    }
+        //}
+
+        //[FunctionName("GetCanaryPete")]
+        //public async Task RunGetCanaryPete([TimerTrigger(SCHEDULE_EXPRESSION)] TimerInfo myTimer)
+        //{
+        //    var lastUpdated = myTimer?.ScheduleStatus?.Last ?? MINIMUM_DATE;
+        //    _logger.LogInformation("Last SyncTime was {SyncTime}", lastUpdated);
+
+        //    var tweets = await _twitterService.GetTweetsFromUserAsync("2605192496", lastUpdated).ConfigureAwait(false);
+        //    var cartoonTweets = tweets.Where(tweet => tweet.Entities?.Hashtags?.Any(t => string.Equals(t?.Tag, "cartoon", StringComparison.OrdinalIgnoreCase) || string.Equals(t?.Tag, "comics", StringComparison.OrdinalIgnoreCase)) == true);
+        //    _logger.LogInformation("Found {TotalTweets} Tweets for cartoonist {Cartoonist}", cartoonTweets.Count(), "CanaryPete");
+        //    foreach (var cartoon in cartoonTweets)
+        //    {
+        //        try
+        //        {
+        //            await _redditService.CreateLinkPostAsync("BelgianCartoons", $"{cartoon.Text.Split("\n")[0]}", $"https://twitter.com/canarypete2/status/{cartoon.Id}?", "CanaryPete");
+        //        }
+        //        catch (Exception ex)
+        //        {
+        //            _logger.LogError(ex, "Problem with processing Tweet {Tweet} for cartoonist {Cartoonist}", cartoon.Id, "CanaryPete");
+        //        }
+        //    }
+        //}
+
+        //[FunctionName("GetFaudtnieuws")]
+        //public async Task RunGetFaudtNieuws([TimerTrigger(SCHEDULE_EXPRESSION)] TimerInfo myTimer)
+        //{
+        //    var lastUpdated = myTimer?.ScheduleStatus?.Last ?? MINIMUM_DATE;
+        //    _logger.LogInformation("Last SyncTime was {SyncTime}", lastUpdated);
+
+        //    var tweets = await _twitterService.GetTweetsFromUserAsync("3257053183", lastUpdated).ConfigureAwait(false);
+        //    var cartoonTweets = tweets.Where(tweet => tweet.Entities?.Hashtags?.Any(t => string.Equals(t?.Tag, "faudtnieuws", StringComparison.OrdinalIgnoreCase)) == true);
+        //    _logger.LogInformation("Found {TotalTweets} Tweets for cartoonist {Cartoonist}", cartoonTweets.Count(), "FaudtNieuws");
+        //    foreach (var cartoon in cartoonTweets)
+        //    {
+        //        try
+        //        {
+        //            await _redditService.CreateLinkPostAsync("BelgianCartoons", $"{cartoon.Text.Split("\n")[0]}", $"https://twitter.com/faudtnieuws/status/{cartoon.Id}?", "FaudtNieuws");
+        //        }
+        //        catch (Exception ex)
+        //        {
+        //            _logger.LogError(ex, "Problem with processing Tweet {Tweet} for cartoonist {Cartoonist}", cartoon.Id, "FaudtNieuws");
+        //        }
+        //    }
+        //}
+
+        //[FunctionName("GetJeroomCartoons")]
+        //public async Task RunGetJeroomCartoons([TimerTrigger(SCHEDULE_EXPRESSION)] TimerInfo myTimer)
+        //{
+        //    var lastUpdated = myTimer?.ScheduleStatus?.Last ?? MINIMUM_DATE;
+        //    _logger.LogInformation("Last SyncTime was {SyncTime}", lastUpdated);
+
+        //    var tweets = await _twitterService.GetTweetsFromUserAsync("27617210", lastUpdated).ConfigureAwait(false);
+        //    var cartoonTweets = tweets.Where(tweet => tweet.Text.Contains("Jeroom", StringComparison.OrdinalIgnoreCase));
+        //    _logger.LogInformation("Found {TotalTweets} Tweets for cartoonist {Cartoonist}", cartoonTweets.Count(), "Jeroom");
+        //    foreach (var cartoon in cartoonTweets)
+        //    {
+        //        try
+        //        {
+        //            await _redditService.CreateLinkPostAsync("BelgianCartoons", $"{cartoon.Text}", $"https://twitter.com/Humo/status/{cartoon.Id}?", "Jeroom");
+        //        }
+        //        catch (Exception ex)
+        //        {
+        //            _logger.LogError(ex, "Problem with processing Tweet {Tweet} for cartoonist {Cartoonist}", cartoon.Id, "Jeroom");
+        //        }
+        //    }
+        //}
+
+        //[FunctionName("GetKimDuchateau")]
+        //public async Task RunGetKimDuchateau([TimerTrigger(SCHEDULE_EXPRESSION)] TimerInfo myTimer)
+        //{
+        //    var lastUpdated = myTimer?.ScheduleStatus?.Last ?? MINIMUM_DATE;
+        //    _logger.LogInformation("Last SyncTime was {SyncTime}", lastUpdated);
+
+        //    var cartoonTweets = await _twitterService.GetTweetsFromUserAsync("3351533926", lastUpdated).ConfigureAwait(false);
+        //    _logger.LogInformation("Found {TotalTweets} Tweets for cartoonist {Cartoonist}", cartoonTweets.Count(), "KimDuchateau");
+        //    foreach (var cartoon in cartoonTweets)
+        //    {
+        //        try
+        //        {
+        //            await _redditService.CreateLinkPostAsync("BelgianCartoons", $"{cartoon.Text}", $"https://twitter.com/kim_duchateau/status/{cartoon.Id}", "KimDuchateau");
+        //        }
+        //        catch (Exception ex)
+        //        {
+        //            _logger.LogError(ex, "Problem with processing Tweet {Tweet} for cartoonist {Cartoonist}", cartoon.Id, "Jeroom");
+        //        }
+        //    }
+        //}
+
+        //[FunctionName("GetLectrrCartoons")]
+        //public async Task RunGetLectrrCartoons([TimerTrigger(SCHEDULE_EXPRESSION)] TimerInfo myTimer)
+        //{
+        //    var lastUpdated = myTimer?.ScheduleStatus?.Last ?? MINIMUM_DATE;
+        //    _logger.LogInformation("Last SyncTime was {SyncTime}", lastUpdated);
+
+        //    var tweets = await _twitterService.GetTweetsFromUserAsync("13157492", lastUpdated).ConfigureAwait(false);
+        //    var cartoonTweets = tweets.Where(tweet => tweet.Entities?.Hashtags?.Any(t => string.Equals(t?.Tag, "cartoon", StringComparison.OrdinalIgnoreCase)) == true);
+        //    _logger.LogInformation("Found {TotalTweets} Tweets for cartoonist {Cartoonist}", cartoonTweets.Count(), "Lectrr");
+        //    foreach (var cartoon in cartoonTweets)
+        //    {
+        //        try
+        //        {
+        //            await _redditService.CreateLinkPostAsync("BelgianCartoons", $"{cartoon.Text}", $"https://twitter.com/lectrr/status/{cartoon.Id}?", "Lectrr");
+        //        }
+        //        catch (Exception ex)
+        //        {
+        //            _logger.LogError(ex, "Problem with processing Tweet {Tweet} for cartoonist {Cartoonist}", cartoon.Id, "Lectrr");
+        //        }
+        //    }
+        //}
+
+        [FunctionName("GetVanmoltoons")]
+        public async Task RunGetVanmolCartoons([TimerTrigger(SCHEDULE_EXPRESSION)] TimerInfo myTimer)
         {
             var lastUpdated = myTimer?.ScheduleStatus?.Last ?? MINIMUM_DATE;
             _logger.LogInformation("Last SyncTime was {SyncTime}", lastUpdated);
 
-            var tweets = await _twitterService.GetTweetsFromUserAsync("25047751", lastUpdated).ConfigureAwait(false);
-            var cartoonTweets = tweets.Where(tweet => tweet.Entities?.Hashtags?.Any(t => string.Equals(t?.Tag, "comic", StringComparison.OrdinalIgnoreCase) || string.Equals(t?.Tag, "comics", StringComparison.OrdinalIgnoreCase)) == true);
-            _logger.LogInformation("Found {TotalTweets} Tweets for cartoonist {Cartoonist}", cartoonTweets.Count(), "Soupcat");
-            foreach (var cartoon in cartoonTweets)
-            {
-                try
-                {
-                    await _redditService.CreateLinkPostAsync("BelgianCartoons", $"{cartoon.Text.Split("\n")[0]}", $"https://twitter.com/soupcats/status/{cartoon.Id}?", "Soupcat");
-                }
-                catch (Exception ex)
-                {
-                    _logger.LogError(ex, "Problem with processing Tweet {Tweet} for cartoonist {Cartoonist}", cartoon.Id, "Soupcat");
-                }
-            }
-        }
-
-        [FunctionName("GetArnulfBe")]
-        public async Task RunGetArnulfBe([TimerTrigger(SCHEDULE_EXPRESSION)] TimerInfo myTimer)
-        {
-            var lastUpdated = myTimer?.ScheduleStatus?.Last ?? MINIMUM_DATE;
-            _logger.LogInformation("Last SyncTime was {SyncTime}", lastUpdated);
-
-            var tweets = await _twitterService.GetTweetsFromUserAsync("249821479", lastUpdated).ConfigureAwait(false);
+            var tweets = await _twitterService.GetTweetsFromUserAsync("34889535", lastUpdated).ConfigureAwait(false);
             var cartoonTweets = tweets.Where(tweet => tweet.Entities?.Hashtags?.Any(t => string.Equals(t?.Tag, "cartoon", StringComparison.OrdinalIgnoreCase)) == true);
-            _logger.LogInformation("Found {TotalTweets} Tweets for cartoonist {Cartoonist}", cartoonTweets.Count(), "ArnulfBE");
+            _logger.LogInformation("Found {TotalTweets} Tweets for cartoonist {Cartoonist}", cartoonTweets.Count(), "Vanmoltoons");
             foreach (var cartoon in cartoonTweets)
             {
                 try
                 {
-                    await _redditService.CreateLinkPostAsync("BelgianCartoons", $"{cartoon.Text.Split("\n")[0]}", $"https://twitter.com/arnulfcartoon/status/{cartoon.Id}?", "Arnulf");
+                    await _redditService.CreateLinkPostAsync("BelgianCartoons", $"{cartoon.Text}", $"https://twitter.com/Vanmoltoons/status/{cartoon.Id}?", "Lectrr");
                 }
                 catch (Exception ex)
                 {
-                    _logger.LogError(ex, "Problem with processing Tweet {Tweet} for cartoonist {Cartoonist}", cartoon.Id, "Arnulf");
-                }
-            }
-        }
-
-        [FunctionName("GetCanaryPete")]
-        public async Task RunGetCanaryPete([TimerTrigger(SCHEDULE_EXPRESSION)] TimerInfo myTimer)
-        {
-            var lastUpdated = myTimer?.ScheduleStatus?.Last ?? MINIMUM_DATE;
-            _logger.LogInformation("Last SyncTime was {SyncTime}", lastUpdated);
-
-            var tweets = await _twitterService.GetTweetsFromUserAsync("2605192496", lastUpdated).ConfigureAwait(false);
-            var cartoonTweets = tweets.Where(tweet => tweet.Entities?.Hashtags?.Any(t => string.Equals(t?.Tag, "cartoon", StringComparison.OrdinalIgnoreCase) || string.Equals(t?.Tag, "comics", StringComparison.OrdinalIgnoreCase)) == true);
-            _logger.LogInformation("Found {TotalTweets} Tweets for cartoonist {Cartoonist}", cartoonTweets.Count(), "CanaryPete");
-            foreach (var cartoon in cartoonTweets)
-            {
-                try
-                {
-                    await _redditService.CreateLinkPostAsync("BelgianCartoons", $"{cartoon.Text.Split("\n")[0]}", $"https://twitter.com/canarypete2/status/{cartoon.Id}?", "CanaryPete");
-                }
-                catch (Exception ex)
-                {
-                    _logger.LogError(ex, "Problem with processing Tweet {Tweet} for cartoonist {Cartoonist}", cartoon.Id, "CanaryPete");
-                }
-            }
-        }
-
-        [FunctionName("GetFaudtnieuws")]
-        public async Task RunGetFaudtNieuws([TimerTrigger(SCHEDULE_EXPRESSION)] TimerInfo myTimer)
-        {
-            var lastUpdated = myTimer?.ScheduleStatus?.Last ?? MINIMUM_DATE;
-            _logger.LogInformation("Last SyncTime was {SyncTime}", lastUpdated);
-
-            var tweets = await _twitterService.GetTweetsFromUserAsync("3257053183", lastUpdated).ConfigureAwait(false);
-            var cartoonTweets = tweets.Where(tweet => tweet.Entities?.Hashtags?.Any(t => string.Equals(t?.Tag, "faudtnieuws", StringComparison.OrdinalIgnoreCase)) == true);
-            _logger.LogInformation("Found {TotalTweets} Tweets for cartoonist {Cartoonist}", cartoonTweets.Count(), "FaudtNieuws");
-            foreach (var cartoon in cartoonTweets)
-            {
-                try
-                {
-                    await _redditService.CreateLinkPostAsync("BelgianCartoons", $"{cartoon.Text.Split("\n")[0]}", $"https://twitter.com/faudtnieuws/status/{cartoon.Id}?", "FaudtNieuws");
-                }
-                catch (Exception ex)
-                {
-                    _logger.LogError(ex, "Problem with processing Tweet {Tweet} for cartoonist {Cartoonist}", cartoon.Id, "FaudtNieuws");
-                }
-            }
-        }
-
-        [FunctionName("GetJeroomCartoons")]
-        public async Task RunGetJeroomCartoons([TimerTrigger(SCHEDULE_EXPRESSION)] TimerInfo myTimer)
-        {
-            var lastUpdated = myTimer?.ScheduleStatus?.Last ?? MINIMUM_DATE;
-            _logger.LogInformation("Last SyncTime was {SyncTime}", lastUpdated);
-
-            var tweets = await _twitterService.GetTweetsFromUserAsync("27617210", lastUpdated).ConfigureAwait(false);
-            var cartoonTweets = tweets.Where(tweet => tweet.Text.Contains("Jeroom", StringComparison.OrdinalIgnoreCase));
-            _logger.LogInformation("Found {TotalTweets} Tweets for cartoonist {Cartoonist}", cartoonTweets.Count(), "Jeroom");
-            foreach (var cartoon in cartoonTweets)
-            {
-                try
-                {
-                    await _redditService.CreateLinkPostAsync("BelgianCartoons", $"{cartoon.Text}", $"https://twitter.com/Humo/status/{cartoon.Id}?", "Jeroom");
-                }
-                catch (Exception ex)
-                {
-                    _logger.LogError(ex, "Problem with processing Tweet {Tweet} for cartoonist {Cartoonist}", cartoon.Id, "Jeroom");
-                }
-            }
-        }
-
-        [FunctionName("GetKimDuchateau")]
-        public async Task RunGetKimDuchateau([TimerTrigger(SCHEDULE_EXPRESSION)] TimerInfo myTimer)
-        {
-            var lastUpdated = myTimer?.ScheduleStatus?.Last ?? MINIMUM_DATE;
-            _logger.LogInformation("Last SyncTime was {SyncTime}", lastUpdated);
-
-            var cartoonTweets = await _twitterService.GetTweetsFromUserAsync("3351533926", lastUpdated).ConfigureAwait(false);
-            _logger.LogInformation("Found {TotalTweets} Tweets for cartoonist {Cartoonist}", cartoonTweets.Count(), "KimDuchateau");
-            foreach (var cartoon in cartoonTweets)
-            {
-                try
-                {
-                    await _redditService.CreateLinkPostAsync("BelgianCartoons", $"{cartoon.Text}", $"https://twitter.com/kim_duchateau/status/{cartoon.Id}", "KimDuchateau");
-                }
-                catch (Exception ex)
-                {
-                    _logger.LogError(ex, "Problem with processing Tweet {Tweet} for cartoonist {Cartoonist}", cartoon.Id, "Jeroom");
-                }
-            }
-        }
-
-        [FunctionName("GetLectrrCartoons")]
-        public async Task RunGetLectrrCartoons([TimerTrigger(SCHEDULE_EXPRESSION)] TimerInfo myTimer)
-        {
-            var lastUpdated = myTimer?.ScheduleStatus?.Last ?? MINIMUM_DATE;
-            _logger.LogInformation("Last SyncTime was {SyncTime}", lastUpdated);
-
-            var tweets = await _twitterService.GetTweetsFromUserAsync("13157492", lastUpdated).ConfigureAwait(false);
-            var cartoonTweets = tweets.Where(tweet => tweet.Entities?.Hashtags?.Any(t => string.Equals(t?.Tag, "cartoon", StringComparison.OrdinalIgnoreCase)) == true);
-            _logger.LogInformation("Found {TotalTweets} Tweets for cartoonist {Cartoonist}", cartoonTweets.Count(), "Lectrr");
-            foreach (var cartoon in cartoonTweets)
-            {
-                try
-                {
-                    await _redditService.CreateLinkPostAsync("BelgianCartoons", $"{cartoon.Text}", $"https://twitter.com/lectrr/status/{cartoon.Id}?", "Lectrr");
-                }
-                catch (Exception ex)
-                {
-                    _logger.LogError(ex, "Problem with processing Tweet {Tweet} for cartoonist {Cartoonist}", cartoon.Id, "Lectrr");
+                    _logger.LogError(ex, "Problem with processing Tweet {Tweet} for cartoonist {Cartoonist}", cartoon.Id, "Vanmoltoons");
                 }
             }
         }
