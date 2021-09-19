9caa8b50c7908f01937b9a723558db4c8500ce7b
Echarnus <kenneth.de.clercq@me.com>
2021-09-19 22:08:36 +0200

    Vanmoltoons

--- src/BelgianCartoons.Abstract/Models/Twitter/Entities.cs
+++ src/BelgianCartoons.Abstract/Models/Twitter/Entities.cs
@@ -5,7 +5,7 @@ namespace BelgianCartoons.Abstract.Models.Twitter
     public class Entities
     {
         public IEnumerable<Mention> Mentions { get; set; }
-        public IEnumerable<TwitterUrl> Url { get; set; }
+        public IEnumerable<TwitterUrl> Urls { get; set; }
         public IEnumerable<Annotation> Annotations { get; set; }
         public IEnumerable<Hashtag> Hashtags { get; set; }
     }
